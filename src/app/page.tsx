98d773044ab28125e5f43f01e7bbd180387641be
manojthetechguy <manojthetechguy@gmail.com>
2024-09-01 15:23:45 +0530

    feat: embed twitter timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      Alok Jagawat Tweets Showcase
+      <h3>
+        Alok Jagawat Tweets Showcase
+      </h3>
+      <a className="twitter-timeline" href="https://twitter.com/AlokJagawat?ref_src=twsrc%5Etfw">Tweets by AlokJagawat</a> <script async src="https://platform.twitter.com/widgets.js" ></script>
     </main>
   );
 }
